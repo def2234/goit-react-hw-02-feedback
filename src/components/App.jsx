7081b0cb809c488ff665eab70d5c3e1b36c9f9ad
def2234 <def22349@gmail.com>
2023-03-11 21:00:40 +0100

    Hw Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { FeedBackOptions } from './FeedbackOptins/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = e => {
+    this.setState({
+      [e.target.name]: this.state[e.target.name] + 1,
+    });
+  };
+
+  countTotalFeedback = () => {
+    let total = 0;
+    const arrState = Object.values(this.state);
+    for (let i = 0; i < arrState.length; i += 1) {
+      total += arrState[i];
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+
+    if (total > 0) {
+      return Math.round((this.state.good / total) * 100);
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <Section title={'Please leave feedback'}>
+          <FeedBackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title={'Statistics'}>
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              bad={this.state.bad}
+              neutral={this.state.neutral}
+              total={this.countTotalFeedback()}
+              persentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
